55a386cef992b84f853ea7b5dde8c0319055fed9
Pierre-Marc Airoldi <pierremarcairoldi@gmail.com>
2014-10-23 15:53:50 -0400

    + sign tests

--- prefix_test.go
+++ prefix_test.go
@@ -20,6 +20,16 @@ var prefixTests = []struct {
 	{"420737471423", "Czech Republic"},
 	{"390669812345", "Vatican City"},
 	{"3799812345", "Vatican City"},
+	{"+46485562003", "Sweden"},
+	{"+61296384929", "Australia"},
+	{"+74956034085", "Russia"},
+	{"+14158575430", "United States"},
+	{"+14169323338", "Canada"},
+	{"+12504798004", "Canada"},
+	{"+6493662929", "New Zealand"},
+	{"+420737471423", "Czech Republic"},
+	{"+390669812345", "Vatican City"},
+	{"+3799812345", "Vatican City"},
 }
 
 var invalidPrefixes = []string{
